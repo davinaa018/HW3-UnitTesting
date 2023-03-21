e453e2cc6004e47ee054795f5a4d9df47c22198f
davinaa018 <davinaa018@gmail.com>
2023-03-20 18:48:28 -0600

    Added Testing for isOccupiedBy, playerAt, Horizontal/Verical/Diagonal/Reverse Diagonal methods

--- TestBoard.java
+++ TestBoard.java
@@ -1,7 +1,7 @@
 package HW3;
 
-import org.junit.Assert.*;
 
+import static org.junit.Assert.assertNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -49,6 +49,7 @@ public class TestBoard {
     public void testPlaceStone(){
        board.placeStone(0, 0, player1);
        assertTrue(board.isOccupiedBy(0, 0, player1));
+       assertFalse(board.isOccupiedBy(0, 0, player2));
     }
 
     @Test(expected = IllegalArgumentException.class)
@@ -70,6 +71,125 @@ public class TestBoard {
         board.placeStone(0, 0, player1);
         assertTrue(board.isOccupied(0, 0));
     }
+
+    @Test
+    public void testIsOccupiedBy(){
+        assertFalse(board.isOccupiedBy(0, 0, player1));
+        board.placeStone(0, 0, player1);
+        assertTrue(board.isOccupiedBy(0, 0, player1));
+        assertFalse(board.isOccupiedBy(0, 0, player2));
+    }
+
+    @Test
+    public void testPlayerAt(){
+        assertNull( board.playerAt(0, 0));
+        board.placeStone(0, 0, player2);
+        // assertEquals(player2, board.playerAt(0, 0));
+    }
+
+    @Test
+    public void testHorizontalWin(){
+        assertFalse(board.isWonBy(player1));
+        assertFalse(board.isWonBy(player2));
+    
+        // Horizontal Stones for player1 
+        board.placeStone(0, 0, player1);
+        board.placeStone(1, 0, player1);
+        board.placeStone(2, 0, player1);
+        board.placeStone(3, 0, player1);
+        board.placeStone(4, 0, player1);      
+        assertTrue(board.isWonBy(player1));
+        assertFalse(board.isWonBy(player2));
+        board.clear();
+
+        // Horizontal Stones for player2
+        board.placeStone(0, 1, player2);
+        board.placeStone(1, 1, player2);
+        board.placeStone(2, 1, player2);
+        board.placeStone(3, 1, player2);
+        board.placeStone(4, 1, player2);
+        assertTrue(board.isWonBy(player2));
+        assertFalse(board.isWonBy(player1));
+    }
+
+    @Test
+    public void testVerticalWin(){
+        assertFalse(board.isWonBy(player1));
+        assertFalse(board.isWonBy(player2));
+    
+        // Vertical Stones for player1 
+        board.placeStone(0, 0, player1);
+        board.placeStone(0, 1, player1);
+        board.placeStone(0, 2, player1);
+        board.placeStone(0, 3, player1);
+        board.placeStone(0, 4, player1);      
+        assertTrue(board.isWonBy(player1));
+        assertFalse(board.isWonBy(player2));
+
+        board.clear();
+
+        // Vertical Stones for player2
+        board.placeStone(1, 0, player2);
+        board.placeStone(1, 1, player2);
+        board.placeStone(1, 2, player2);
+        board.placeStone(1, 3, player2);
+        board.placeStone(1, 4, player2);
+        assertTrue(board.isWonBy(player2));
+        assertFalse(board.isWonBy(player1));
+    }
+
+    @Test
+    public void testDiagonalWin(){
+        assertFalse(board.isWonBy(player1));
+        assertFalse(board.isWonBy(player2));
+    
+        // Diagonal Stones for player1 
+        board.placeStone(0, 0, player1);
+        board.placeStone(1, 1, player1);
+        board.placeStone(2, 2, player1);
+        board.placeStone(3, 3, player1);
+        board.placeStone(4, 4, player1);      
+        assertTrue(board.isWonBy(player1));
+        assertFalse(board.isWonBy(player2));
+
+        board.clear();
+
+        // Diagonal Stones for player2
+        board.placeStone(0, 0, player2);
+        board.placeStone(1, 1, player2);
+        board.placeStone(2, 2, player2);
+        board.placeStone(3, 3, player2);
+        board.placeStone(4, 4, player2);      
+        assertTrue(board.isWonBy(player2));
+        assertFalse(board.isWonBy(player1));
+    }
+
+    @Test
+    public void testReverseDiagonalWin(){
+        assertFalse(board.isWonBy(player1));
+        assertFalse(board.isWonBy(player2));
+    
+        // Reverse Diagonal Stones for player1 
+        board.placeStone(0, 4, player1);
+        board.placeStone(1, 3, player1);
+        board.placeStone(2, 2, player1);
+        board.placeStone(3, 1, player1);
+        board.placeStone(4, 0, player1);      
+        assertTrue(board.isWonBy(player1));
+        assertFalse(board.isWonBy(player2));
+
+        board.clear();
+
+        // Reverse Diagonal Stones for player2
+        board.placeStone(0, 4, player2);
+        board.placeStone(1, 3, player2);
+        board.placeStone(2, 2, player2);
+        board.placeStone(3, 1, player2);
+        board.placeStone(4, 0, player2);      
+        assertTrue(board.isWonBy(player2));
+        assertFalse(board.isWonBy(player1));
+    }
+    
 
     
 }
